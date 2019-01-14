df81acc204fa05418b895d8a15dbfa7dd22e058d
wqlab0000 <w.q.lab00@gmail.com>
2019-01-14 01:10:14 +0000

    Update PatientsProfile.java

--- PatientsProfile.java
+++ PatientsProfile.java
@@ -1,4 +1,5 @@
 
+
 //Given("^User is trying to see patient's profile$")
 
 public class PatientsProfile {
@@ -47,5 +48,60 @@ public class PatientsProfile {
         void setProfile(int profile) {
             this.profile = profile;
         }
+    }
+}
+
+
+//When("^user name and password are in the user index$")
+
+
+class UserPasswordFile {
+
+    /*@ public final model \locset footprint; 
+      @ represents footprint =
+      @     \locset(names, passwords), names[*], passwords[*];
+      @*/
+
+    /*@ model int userIndex;
+      @ represents userIndex \such_that
+      @     0 <= userIndex && userIndex < names.length;
+      @ accessible userIndex : \locset(names);
+      @*/
+
+    /*@ invariant   names.length == passwords.length;
+      @*/
+    
+    private int[] names;
+    private int[] passwords;
+
+
+    public SecurePasswordFile(int[] names, int[] passwords) {
+        this.names = names;
+        this.passwords = passwords;
+    }
+
+
+    /**
+     * Returns whether password is correct.
+     */
+    
+ //Then("^user has privilege to the profile$")
+
+    public boolean check(int user, int password) {
+        /*@ loop_invariant
+          @     0 <= i
+          @  && i <= names.length
+          @  && (\forall int j; 0 <= j && j < i;
+          @         !(names[j] == user && passwords[j] == password));
+          @ assignable \nothing;
+          @ decreases names.length - i;
+          @*/
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] == user &&
+                passwords[i] == password) {
+                return true;
+            }
+        }
+        return false;
     }
 }
